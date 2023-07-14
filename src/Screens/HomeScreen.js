2cea1e30a92e418bafd0d49f4b7e8cdeb6a9aee4
Rohit-44 <tomarrohit2408@gmail.com>
2023-07-14 21:55:32 +0530

    Frontend-Ecommerce

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,74 @@
+import { Link } from 'react-router-dom';
+// import data from '../data';
+import axios from 'axios';
+import Rating from '../components/Rating';
+import logger from 'use-reducer-logger';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useEffect, useReducer, useState } from 'react';
+import Product from '../components/Product';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, products: action.payload };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+function HomeScreen() {
+  // const [products, setProducts] = useState([]);
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch {
+        dispatch({ type: 'FETCH_FAIL', payload: error.message });
+      }
+
+      //  setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <Helmet>
+        <title>SwiftCart</title>
+      </Helmet>
+      <h1>Featured products</h1>
+
+      <div className="products">
+        {loading ? (
+          <LoadingBox />
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default HomeScreen;
